427a72735fc63fa666d7cc69582d99ce564937c9
Victor Soares <victor.soares54327@gmail.com>
2026-04-26 20:16:18 -0300

    adicionando data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 AOS.init();
+
+const dataDoEvento = new Date("may 21, 2026 12:00:00");
+const timeStampEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval( function () {
+
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampEvento - timeStampAtual;
+    
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diasEmMs );
+    const horasAteEvento = Math.floor((distanciaAteEvento % diasEmMs) / horasEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horasEmMs) / minutosEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
+
+    document.getElementById('data').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+}, 1000)
